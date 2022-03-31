de613211df8cfd9617a314da143aa5583f8e06e6
Mumtaz7868 <97096663+Mumtaz7868@users.noreply.github.com>
2022-03-31 20:48:13 +0500

    Authentication

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoStore = require("connect-mongo");
+const passport = require("passport");
 
 // var store = new MongoDBStore({
 //   // uri: "mongodb://localhost/pizza",
@@ -42,15 +43,25 @@ app.use(
   })
 );
 
+//Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 //Assets
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Global middleware
+// app.use("express-session");
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
+
   next();
 });
 
